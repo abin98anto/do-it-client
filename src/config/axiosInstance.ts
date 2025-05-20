ac3b6ec9ad38a99094ff0f69686dad37e0eb70a7
abin98anto <abin98anto@gmail.com>
2025-05-20 15:17:24 +0530

    modified BE URL - 3

--- src/config/axiosInstance.ts
+++ src/config/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "https://do-it-server-jsod.onrender.com",
+  baseURL: "https://do-it-server-jsod.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
